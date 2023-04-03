9d20b68afdbae5c664e287b82dcc45630289cb3e
Nehadhruwa <128783091+Nehadhruwa@users.noreply.github.com>
2023-04-03 12:35:04 +0530

    Added Medicine demand, Pharmacy medicine supply

---
+++ PharmacySupplyProject/models/MedicineDemand.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmacySupplyProject.Models
+{
+    public class MedicineDemand
+    {
+        [Key]
+        public string MedicineName { get; set; } = string.Empty;
+        public int DemandCount { get; set; }
+    }
+}
