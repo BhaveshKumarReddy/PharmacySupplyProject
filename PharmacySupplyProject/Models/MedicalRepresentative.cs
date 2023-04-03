3bde3772427fadf598a8c5fdd465d19952f59927
Devaroshan <108619145+Devaroshan@users.noreply.github.com>
2023-04-03 10:15:08 +0530

    MedicalRepresentative model is created

---
+++ PharmacySupplyProject/Models/MedicalRepresentative.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmacySupplyProject.Models
+{
+    public class MedicalRepresentative
+    {
+        public int Id { get; set; }
+        [Key]
+        public string Name { get; set; } = string.Empty;
+        [StringLength(10)]
+        public string ContactNumber { get; set; } = string.Empty;
+    }
+}
