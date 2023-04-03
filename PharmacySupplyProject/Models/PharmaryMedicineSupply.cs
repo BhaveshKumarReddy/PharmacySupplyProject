ca9260608bab3253730205f32b4e7a48012b79a9
Devaroshan <108619145+Devaroshan@users.noreply.github.com>
2023-04-03 12:17:23 +0530

    Added Manager and pharmacymedicinesupply models

---
+++ PharmacySupplyProject/Models/PharmaryMedicineSupply.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmacySupplyProject.Models
+{
+    public class PharmaryMedicineSupply
+    {
+        [Key]
+        public string PharmacyName { get; set; } = string.Empty;
+        public string MedicineName { get; set; } = string.Empty;
+        public int SupplyCount { get; set; }
+    }
+}
